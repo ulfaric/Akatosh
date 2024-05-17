c8c06ff236c49fb2421a769c63f910bbe043efd6
ulfaric <ryf0510@live.com>
2024-05-17 09:14:59 +0800

    updated

--- test_event_after_event.py
+++ test_event_after_event.py
@@ -4,9 +4,9 @@ import Akatosh
 from Akatosh.event import Event
 from Akatosh.universe import Mundus
 
-hello = Event(0.5, 0.5, lambda: print(f"Hello at {Mundus.time}"))
-world = Event(hello, 0.5, lambda: print(f"World at {Mundus.time}"))
+hello = Event(0, 5, lambda: print(f"Hello at {Mundus.time}"))
+world = Event(hello, 10, lambda: print(f"World at {Mundus.time}"))
 
 Mundus.enable_realtime()
 Akatosh.logger.setLevel(logging.DEBUG)
-asyncio.run(Mundus.simulate(0.51))
+asyncio.run(Mundus.simulate(10))
